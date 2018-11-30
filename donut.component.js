69facf9aedd7dd3952f58995af5cbf6a9f1978c1
rogeraustinsam <44400954+rogeraustinsam@users.noreply.github.com>
2018-11-30 11:43:49 -0500

    Update donut.component.js

--- donut.component.js
+++ donut.component.js
@@ -53,10 +53,7 @@ var DonutComponent = /** @class */ (function () {
                 columns: [
                     ['storage', 91.4]
                 ],
-                type: 'gauge',
-                onclick: function (d, i) { console.log("onclick", d, i); },
-                onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-                onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+                type: 'gauge'
             },
             color: {
                 pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'],
